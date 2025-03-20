0f0542da008510258b3f98bd1fd3fa86977e102a
Ruben Balaguer <rbalaguerpico@gmail.com>
2025-03-20 19:27:23 +0100

    Televisio fet

---
+++ src/sa8/a15/gestioaparells/Televisio.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sa8.a15.gestioaparells;
+
+/**
+ *
+ * @author batoi
+ */
+public class Televisio extends Electrodomestic{
+    
+    private boolean ences;
+    private boolean estaWifiHabilitat;
+    private boolean estaConectadaAInternet;
+    private double resolucio;
+    
+    public Televisio(boolean estaConectadaAInternet, double resolucio, int preBase, color colorElectro, consumEnergetic lletraConsum, double pes, boolean estWifiHabilitat ,boolean ences) {
+        super(preBase, colorElectro, lletraConsum, pes);
+        this.ences = ences;
+        this.estaWifiHabilitat = estaWifiHabilitat;
+        this.estaConectadaAInternet = estaConectadaAInternet;
+        this.resolucio = resolucio;
+    }
+    
+    public Televisio(int preBase, boolean estaConectadaAInternet, double resolucio, color colorElectro, consumEnergetic lletraConsum, boolean estWifiHabilitat ,boolean ences, double pes) {
+        super(preBase, colorElectro, lletraConsum, pes);
+        this.ences = false;
+        this.estaWifiHabilitat = false;
+        this.estaConectadaAInternet = false;
+        this.resolucio = 20;
+    }
+    
+    @Override
+    public double obtindrePreuVenda(){
+        if(resolucio >= 40){
+            return super.obtindrePreuVenda() * 0.3;
+        }
+        return super.obtindrePreuVenda();
+    }
+    
+    
+    
+}
